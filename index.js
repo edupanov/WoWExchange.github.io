e27bb1f4bca3f5194a7b46827f5b9edc7f65b5dd
edupanov <edupanov@gmail.com>
2020-04-29 08:51:27 +0300

    work with mpdal window

--- index.js
+++ index.js
@@ -8,8 +8,50 @@ document.addEventListener('DOMContentLoaded', () => {
     const blockChoice = document.getElementById('block-choice');
     const btnExit = document.getElementById('btn-exit');
     const formCustomer = document.getElementById('form-customer');
+    const ordersTable = document.getElementById('orders');
+    const modalOrder = document.getElementById('order_read');
+    const modalOrderActive = document.getElementById('order_active');
 
-    const orders = {};
+    const orders = [];
+
+    const renderOrders = () => {
+        ordersTable.textContent = '';
+        orders.forEach((order, i) => {
+            ordersTable.innerHTML += `
+                <tr class="order" data-number-order = "${i}">
+                    <td>${i + 1}</td>
+                    <td>${order.title}</td>
+                    <td class="${order.currency}"></td>
+                    <td>${order.deadline}</td>
+                </tr>`
+        });
+    };
+
+    const openModal = (numberOrder) => {
+        const order = orders[numberOrder];
+        const modal = order.active ? modalOrderActive : modalOrder;
+        const firstNameBlock = document.querySelector('.firstName');
+        const titleBlock = document.querySelector('.modal-title');
+        const emailBlock = document.querySelector('.email');
+        const descriptionBlock = document.querySelector('.description');
+        const deadlineBlock = document.querySelector('.deadline');
+        const currencyBlock = document.querySelector('.currency_img');
+        const countBlock = document.querySelector('.count');
+        const phoneBlock = document.querySelector('.phone');
+
+        titleBlock.textContent = order.title;
+
+        modal.style.display = 'block';
+    };
+
+    ordersTable.addEventListener('click', (event) => {
+        const target = event.target;
+        const targetOrder = target.closest('.order');
+        if(targetOrder) {
+            openModal(targetOrder.dataset.numberOrder);
+        }
+    });
+
 
     customer.addEventListener('click', () => {
         blockChoice.style.display = 'none';
@@ -19,6 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     freelancer.addEventListener('click', () => {
         blockChoice.style.display = 'none';
+        renderOrders();
         blockFreelancer.style.display = 'block';
         btnExit.style.display = 'block';
     });
@@ -30,24 +73,23 @@ document.addEventListener('DOMContentLoaded', () => {
         blockChoice.style.display = 'block';
     });
 
-    formCustomer.addEventListener ('submit', (event) => {
+    formCustomer.addEventListener('submit', (event) => {
         event.preventDefault();
 
         const obj = {};
 
-        for (const elem of formCustomer.elements) {
-            if (elem.tagName === 'INPUT' && elem.type !== 'radio' ||
+        [...formCustomer.elements].forEach((elem) => {
+            if ((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
                 (elem.type === 'radio' && elem.checked) ||
                 elem.tagName === 'TEXTAREA') {
                 obj [elem.name] = elem.value;
             }
+        });
 
-                 if (elem.type !== 'radio') {
-                     elem.value = '';
-            }
-        }
-
+        formCustomer.reset();
         orders.push(obj);
     });
+
+
 });
 
