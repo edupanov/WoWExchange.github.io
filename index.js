255a84acbc62b67e0f5bfdb0aa9d296f19ba22c1
edupanov <edupanov@gmail.com>
2020-05-04 13:59:17 +0300

    full progect

--- index.js
+++ index.js
@@ -11,50 +11,147 @@ document.addEventListener('DOMContentLoaded', () => {
     const ordersTable = document.getElementById('orders');
     const modalOrder = document.getElementById('order_read');
     const modalOrderActive = document.getElementById('order_active');
+    const headTable = document.getElementById('headTable');
 
-    const orders = [];
+// get from localStorage
+    const orders = JSON.parse(localStorage.getItem('freeOrders')) || [];
+
+// save in localStorage
+    const toStorage = () => {
+        localStorage.setItem('freeOrders', JSON.stringify(orders))
+    };
+
+    const declOfNum = (number, titles) => number + ' ' + titles[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+
+
+    const calcDeadline = (date) => {
+        const deadline = new Date(date);
+        const toDay = Date.now();
+
+        const remaining = (deadline - toDay) / 1000 / 60 / 60;
+
+        if (remaining / 24 > 2) {
+            return declOfNum(Math.floor(remaining / 24), ['день', 'дня', 'дней']);
+        }
+        return declOfNum(Math.floor(remaining), ['час', 'часа', 'часов']);
+    };
 
     const renderOrders = () => {
         ordersTable.textContent = '';
         orders.forEach((order, i) => {
             ordersTable.innerHTML += `
-                <tr class="order" data-number-order = "${i}">
+                <tr class="order ${order.active ? 'taken' : ''}" data-number-order = "${i}">
                     <td>${i + 1}</td>
                     <td>${order.title}</td>
                     <td class="${order.currency}"></td>
-                    <td>${order.deadline}</td>
+                    <td>${calcDeadline(order.deadline)}</td>
                 </tr>`
         });
     };
 
+// event delegation
+    const handlerModal = (event) => {
+        const target = event.target;
+        const modal = target.closest('.order-modal');
+        const order = orders[modal.numberOrder];
+
+        const baseAction = () => {
+            modal.style.display = 'none';
+            toStorage();
+            renderOrders();
+        };
+
+        if (target.closest('.close') || target === modal) {
+            modal.style.display = 'none'
+        }
+
+        if (target.classList.contains('get-order')) {
+            order.active = true;
+            baseAction();
+        }
+
+        if (target.id === 'capitulation') {
+            order.active = false;
+            baseAction();
+        }
+
+        if (target.id === 'ready') {
+            orders.splice(orders.indexOf(order), 1);
+            baseAction();
+        }
+    };
+
+// modal window
     const openModal = (numberOrder) => {
         const order = orders[numberOrder];
-        const modal = order.active ? modalOrderActive : modalOrder;
-        const firstNameBlock = document.querySelector('.firstName');
-        const titleBlock = document.querySelector('.modal-title');
-        const emailBlock = document.querySelector('.email');
-        const descriptionBlock = document.querySelector('.description');
-        const deadlineBlock = document.querySelector('.deadline');
-        const currencyBlock = document.querySelector('.currency_img');
-        const countBlock = document.querySelector('.count');
-        const phoneBlock = document.querySelector('.phone');
+        const {title, firstName, email, phone, description, amount, currency, deadline, active = false} = order;
 
-        titleBlock.textContent = order.title;
+        const modal = active ? modalOrderActive : modalOrder;
 
-        modal.style.display = 'block';
+        const firstNameBlock = modal.querySelector('.firstName');
+        const titleBlock = modal.querySelector('.modal-title');
+        const emailBlock = modal.querySelector('.email');
+        const descriptionBlock = modal.querySelector('.description');
+        const deadlineBlock = modal.querySelector('.deadline');
+        const currencyBlock = modal.querySelector('.currency_img');
+        const countBlock = modal.querySelector('.count');
+        const phoneBlock = modal.querySelector('.phone');
+
+// data output to modal
+        modal.numberOrder = numberOrder;
+        titleBlock.textContent = title;
+        firstNameBlock.textContent = firstName;
+        emailBlock.textContent = email;
+        emailBlock.href = 'mailto:' + email;
+        descriptionBlock.textContent = description;
+        deadlineBlock.textContent = calcDeadline(deadline);
+        currencyBlock.className = 'currency_img';
+        currencyBlock.classList.add(currency);
+        countBlock.textContent = amount;
+        phoneBlock ? phoneBlock.href = "tel:" + phone : '';
+
+        modal.style.display = 'flex';
+
+        modal.addEventListener('click', handlerModal)
     };
+
+// freelancer sort orders
+    const sortOrder = (arr, property) => {
+        arr.sort((a, b) => a[property] > b[property] ? 1 : -1)
+    };
+
+    headTable.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target.classList.contains('head-sort')) {
+            if (target.id === 'taskSort') {
+                sortOrder(orders, 'title')
+            }
+
+            if (target.id ==='currencySort') {
+                sortOrder(orders, 'currency')
+            }
+
+            if (target.id ==='deadlineSort') {
+                sortOrder(orders, 'deadline')
+            }
+            toStorage();
+            renderOrders()
+        }
+    });
 
     ordersTable.addEventListener('click', (event) => {
         const target = event.target;
         const targetOrder = target.closest('.order');
-        if(targetOrder) {
+        if (targetOrder) {
             openModal(targetOrder.dataset.numberOrder);
         }
     });
 
-
     customer.addEventListener('click', () => {
         blockChoice.style.display = 'none';
+        const toDay = new Date().toISOString().substring(0, 10);
+        document.getElementById('deadline').min = toDay;
         blockCustomer.style.display = 'block';
         btnExit.style.display = 'block';
     });
@@ -88,8 +185,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         formCustomer.reset();
         orders.push(obj);
+        toStorage();
     });
-
-
 });
+
 
